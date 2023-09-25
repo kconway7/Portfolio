73f27b322b04d9bfb881780eedb9f8370bf4050a
kconway7 <keddyconway59@gmail.com>
2023-09-25 17:39:35 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Portfolio Website
+
+A custom made portfolio website for myself using React, currently being designed.
---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      Hello! My name is Keddy Conway, and this is my portfolio website
+      (eventually!)
+    </div>
+  );
+}
+
+export default App;
